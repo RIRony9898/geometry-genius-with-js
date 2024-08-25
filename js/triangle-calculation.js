173e726b59e2f6a10ef0d9df006142913b43639d
RIRony9898 <rirony59@gmail.com>
2024-08-25 11:31:00 +0600

    first commit

---
+++ js/triangle-calculation.js
@@ -0,0 +1,21 @@
+function triangleCalculation() {
+  // Get triangle base value
+  const triangleBaseInput = document.getElementById("triangle-base");
+  const triangleBaseText = triangleBaseInput.value;
+  const base = parseFloat(triangleBaseText);
+  //   console.log(base);
+
+  //   get triangle height value
+  const triangleHeightInput = document.getElementById("triangle-height");
+  const triangleHeightText = triangleHeightInput.value;
+  const height = parseFloat(triangleHeightText);
+  //   console.log(height);
+
+  //   calculate triangle area
+  const area = 0.5 * base * height;
+//   console.log(area);
+
+  //   display triangle area
+  const triangleArea = document.getElementById("triangle-area");
+  triangleArea.innerText = area;
+}
